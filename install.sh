e2ba39999ff00a30a0ae9e28d21ac3d0335ca3e3
Junya <junjia.liu@pm.me>
2025-06-23 00:58:48 +0100

    Add hyprshot

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ fi
 read -n1 -rep "would you like to install required packages? (y/n)" INST
 if [[ $INST == "Y" || $INST == "y" ]]; then
   # yay installation goes here basically :p (oh boy its gunna be long)
-  yay -S --noconfirm zsh fastfetch hyfetch kitty pokemon-colorscripts-git hyprpaper wofi ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-font-awesome ttf-jetbrains-mono-nerd
+  yay -S --noconfirm zsh fastfetch hyfetch kitty pokemon-colorscripts-git hyprpaper hyprshot hyprpicker wofi ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-font-awesome ttf-jetbrains-mono-nerd
 
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
